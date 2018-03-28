a65ca8422bbfa02a4d8028e3c33262933eff8a1f
LIU Wei <liuwei@nbu.edu.cn>
2018-03-28 13:27:05 +0800

    完成注册 regin.vue 功能

--- VueAndElementUI/src/components/header.vue
+++ VueAndElementUI/src/components/header.vue
@@ -39,24 +39,77 @@
 
       </el-menu>
     </el-col>
-    <!-- 右边用户信息以及登陆注册 -->
-    <el-button-group>
-      <el-button type="danger" size="small" round >login</el-button>
-      <el-button type="success" size="small" round >regin</el-button>
-    </el-button-group>
+   <!-- 右边用户信息以及登陆注册 -->
+    <el-col :span="4" class="user">
+      <!-- 根据logined值的真假来判断是显示登录按钮还是用户信息
+      以后根据登录状态来改变 -->
+      <el-button-group v-if="!logined" >
+        <el-button  class="button" @click.native="tologin" type="danger" size="small" round >login</el-button>
+        <el-button  class="button" @click.native="toregin" type="success" size="small" round >regin</el-button>
+      </el-button-group>
+      <div v-else>
+        <el-dropdown>
+          <img class="avatar" src="../assets/avatar.jpg" alt="">
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>李小小</el-dropdown-item>
+            <el-dropdown-item>我的工作台</el-dropdown-item>
+            <el-dropdown-item divided>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-col>
   </el-row>
 
 </template>
 <script>
 export default {
+  // ...
+  data () {
+    return {
+      logined: false
+    }
+  },
   methods: {
     handleSelect () {
-      console.log('菜单选择之后的回调操作')
+      console.log()
+    },
+    tohome () {
+      this.$router.push('/')
+      console.log('home')
+    },
+    tologin () {
+      this.$router.push('/login')
+    },
+    toregin () {
+      this.$router.push('/regin')
     }
   }
 }
 </script>
-
 <style scoped>
-
+.header {
+  width: 100%;
+  height: 60px;
+  margin: 0;
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-shadow: 0 0 25px #666;
+}
+.logo img {
+  width: 60px;
+  height: 60px;
+  cursor: pointer;
+}
+.button {
+  margin: 15px 0;
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  margin: 10px 0;
+  cursor: pointer;
+}
 </style>
